01a2bf53c9bf3e0538c533880e2e0cfe8e74c58d
hwan <hwani163@naver.com>
2015-04-23 16:01:24 +0900

    .

--- node1/src/ex1/test05.js
+++ node1/src/ex1/test05.js
@@ -4,13 +4,21 @@
 
 var http = require('http');
 /*
- * 응답헤더 다루기*/
+ * 요청 파라메터 알아 내기*/
 function requestListener(req , res) {
+	//헤더 정보 알아내기
+	console.log(req.headers);
+	
+	//HTTP요청 방식 알아내기
+	console.log("method = ",req.method);
+	
+	// 3) 요청 URL알아내기
+	console.log("URL = ",req.url);
 	res.writeHead(200, {
 		'Content-Type': 'text/html;charset=UTF-8',
 		});
-	res.write("<html><head><title>test04</title></head>");
-	res.write("<body><h1>test04</h1>\n");
+	res.write("<html><head><title>test05</title></head>\n");
+	res.write("<body><h1>test05</h1>\n");
 	res.write("<p>안녕하세요</p>\n");
 	res.end("</body></html>");
 }
